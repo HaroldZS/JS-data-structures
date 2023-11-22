0a9d458588069ae17d88854492565b5040be2a11
haroldZS <haroldzs1024@gmail.com>
2023-11-22 17:35:08 -0400

    Tree search data

--- tree/tree.js
+++ tree/tree.js
@@ -12,7 +12,7 @@ class MyBinarySearchTree {
   }
 
   insert(value) {
-    const newNode = new Node();
+    const newNode = new Node(value);
     if (!this.root) {
       this.root = newNode;
     } else {
@@ -20,7 +20,7 @@ class MyBinarySearchTree {
       while (true) {
         if (value < current.value) {
           if (!current.left) {
-            currentNode.left = newNode;
+            current.left = newNode;
             return this;
           }
           current = current.left;
@@ -30,6 +30,31 @@ class MyBinarySearchTree {
             return this;
           }
           current = current.right;
+        }
+      }
+    }
+  }
+
+  search(value) {
+    if (!this.root) {
+      return undefined;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (current.value === value) {
+          return current;
+        } else {
+          if (value < current.value) {
+            if (!current.left) {
+              return undefined;
+            }
+            current = current.left;
+          } else {
+            if (!current.right) {
+              return undefined;
+            }
+            current = current.right;
+          }
         }
       }
     }
@@ -44,3 +69,4 @@ console.log(myBinarySearchTree.insert(2));
 console.log(myBinarySearchTree.insert(8));
 console.log(myBinarySearchTree.insert(17));
 console.log(myBinarySearchTree.insert(170));
+console.log(myBinarySearchTree.search(24));
